7a9cb113657f88fdd427bcd2f6a91ba746f3e3df
DavigJ <david.j.jung@gmail.com>
2024-07-16 09:44:42 +0400

    stack sizes and containers, fixes #1

--- src/main/java/com/davigj/fly_high/core/registry/FHItems.java
+++ src/main/java/com/davigj/fly_high/core/registry/FHItems.java
@@ -3,6 +3,7 @@ package com.davigj.fly_high.core.registry;
 import com.davigj.fly_high.common.item.FlyBottleItem;
 import com.davigj.fly_high.core.FlyHigh;
 import com.teamabnormals.blueprint.core.util.item.CreativeModeTabContentsPopulator;
+import com.teamabnormals.buzzier_bees.core.registry.BBItems;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.fml.ModList;
@@ -18,10 +19,10 @@ import static net.minecraft.world.item.crafting.Ingredient.of;
 public class FHItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FlyHigh.MOD_ID);
 
-    public static final RegistryObject<Item> BOTTLE_OF_FLY = ITEMS.register("fly_bottle", () -> new FlyBottleItem(new Item.Properties()));
+    public static final RegistryObject<Item> BOTTLE_OF_FLY = ITEMS.register("fly_bottle", () -> new FlyBottleItem(new Item.Properties().craftRemainder(Items.GLASS_BOTTLE).stacksTo(1)));
 
     public static void buildCreativeTabContents() {
-        if (ModList.get().isLoaded("buzzier_bees") && ModList.get().isLoaded("blueprint")) {
+        if (ModList.get().isLoaded("buzzier_bees")) {
             CreativeModeTabContentsPopulator.mod(FlyHigh.MOD_ID)
                     .tab(TOOLS_AND_UTILITIES)
                     .addItemsAfter(of(Items.GLASS_BOTTLE), BOTTLE_OF_FLY);
